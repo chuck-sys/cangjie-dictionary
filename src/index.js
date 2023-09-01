71c0670e824713ed19c26d291221553b4f401165
Cheuk Yin Ng <chucksys88@gmail.com>
2023-09-01 01:26:35 -0700

    feat: mobile support
    - use type=search
    - use both onkeyup and oninput events
    - async load js

--- src/index.js
+++ src/index.js
@@ -2,24 +2,30 @@ const cangjie = require('./cangjie5.json');
 
 const alphabet = "日月金木水火土竹戈十大中一弓人心手口尸廿山女田難卜重";
 
+/**
+ * Convert a sequence of keyboard letters into Chinese characters.
+ */
 function lettersToAlphabet(letters) {
-  let ret = ""
-  for (const c of letters) {
-    let i = c.charCodeAt(0) - 97;
-    if (i < alphabet.length) {
-      ret += alphabet[i];
+    let ret = ""
+    for (const c of letters) {
+        let i = c.charCodeAt(0) - 97;
+        if (i < alphabet.length) {
+            ret += alphabet[i];
+        }
     }
-  }
 
-  return ret;
+    return ret;
 }
 
+/**
+ * Get the sequence of keyboard letters that create a single Chinese character.
+ */
 function toCode(c) {
-  if (cangjie.hasOwnProperty(c)) {
-    return cangjie[c].map(lettersToAlphabet);
-  } else {
-    return [];
-  }
+    if (cangjie.hasOwnProperty(c)) {
+        return cangjie[c].map(lettersToAlphabet);
+    } else {
+        return [];
+    }
 }
 
 function createEntry(c, defList) {
@@ -28,17 +34,29 @@ function createEntry(c, defList) {
         ' : <span class="codes">' + joint + '</span><br>';
 }
 
-function onKeyUp(e) {
-  if (e.code === 'Enter') {
+function onInput() {
     let q = document.getElementById('query').value;
     let resultBox = document.getElementById('result');
 
     resultBox.innerHTML = q.split('')
-      .map(c => createEntry(c, toCode(c)))
-      .join('');
-  }
+        .map(c => createEntry(c, toCode(c)))
+        .join('');
 }
 
-document.getElementById('query').addEventListener('keyup', (e) => {
-  onKeyUp(e);
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(() => {
+    const searchInput = document.getElementById('query');
+    query.addEventListener('input', onInput);
+    query.addEventListener('keyup', e => {
+        if (e.code === 'Enter') {
+            onInput();
+        }
+    });
 });
